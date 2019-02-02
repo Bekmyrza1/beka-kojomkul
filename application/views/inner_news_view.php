55235dac6bab3839a1b44ebfe439bdafd864026d
pryanik1 <alymkulov64@gmail.com>
2019-02-02 13:38:57 +0600

    Надо сделать слайдер

--- application/views/inner_news_view.php
+++ application/views/inner_news_view.php
@@ -1,4 +1,3 @@
-		<?php foreach ($md_news as $news): ?>
 			<div class="pictureundermenu">
 				<div class="container">
 					<div class="themescriptonpicture">
@@ -6,17 +5,23 @@
 					</div>
 				</div>
 			</div>
+		<?php foreach ($md_news as $news): ?>
 			<div class="col-md-8">
 				<div class="row">
 						<div class="borderblog">
-				<h1><?php echo $news['tema_kg'] ?></h1>
+				<h1 id="tema"><?php echo $news['tema_kg'] ?></h1>
 
 				<div class="row">
-					<div class="slider-for">
-						<div class="img" style="background-image: url(<?php echo base_url().$news['foto'];?>);"></div>
+					<div class="slider-for" id="slider-for">
+						<?php 
+			$img_name=$news['gallery'];
+			$arr_js="var fruits ='".$img_name."';";
+			// echo '<div id="img" class="img" style="background-image: url('.base_url()..');"></div>';
+			 ?>
 					</div>
+
 					<div class="slider-nav">
-						<div class="img2" style="background-image: url(<?php echo base_url().$news['foto'];?>);"></div>
+						<div class="img2" style="background-image: url();"></div>
 					</div>
 				</div>
 				<p><?php echo $news['page_text_kg']; ?></p>
@@ -27,5 +32,15 @@
 			</div>
 				</div>
 		
+					<script type="text/javascript">
+<?php echo $arr_js;?>
+var ggg = 456;
+console.log('asdasdasdasd');
+var arr = fruits.split(',');
+for (var i = 0; i < arr.length; i++) {
+var block = document.getElementById('slider-for');
+	block.innerHTML='<div class="img11" style="background-image: url('<?php echo base_url();?>+arr[i]');"></div>';
+}
+		</script>
 			<?php endforeach ?>
 		</div>
